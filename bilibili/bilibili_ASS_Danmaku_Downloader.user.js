faa7610b443efdeba60f2c308e7dd6a2bea5d299
田生 <tiansheng@ya.ru>
2014-06-07 21:03:47 +0800

    文件头

--- bilibili/bilibili_ASS_Danmaku_Downloader.user.js
+++ bilibili/bilibili_ASS_Danmaku_Downloader.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        bilibili ASS Danmaku Downloader
 // @namespace   https://github.com/tiansh
-// @description 以 ASS 格式下载 bilibili 上的弹幕
-// @include     /^http://www\.bilibili\.tv/video/.*$/
-// @include     /^http://bilibili\.kankanews\.com/video/.*$/
+// @description 以 ASS 格式下载 bilibili 的弹幕
+// @include     http://www.bilibili.tv/video/*
+// @include     http://bilibili.kankanews.com/video/*
 // @updateURL   https://tiansh.github.io/us-danmaku/bilibili/bilibili_ASS_Danmaku_Downloader.meta.js
 // @downloadURL https://tiansh.github.io/us-danmaku/bilibili/bilibili_ASS_Danmaku_Downloader.user.js
 // @version     0.5
