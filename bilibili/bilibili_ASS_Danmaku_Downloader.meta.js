c2dbc02c72e6055e935b3474af425ed188824a4d
田生 <tiansheng111@gmail.com>
2014-06-17 18:17:32 +0800

    B1.3: 支持 bilibili.com 域名

--- bilibili/bilibili_ASS_Danmaku_Downloader.meta.js
+++ bilibili/bilibili_ASS_Danmaku_Downloader.meta.js
@@ -2,11 +2,12 @@
 // @name        bilibili ASS Danmaku Downloader
 // @namespace   https://github.com/tiansh
 // @description 以 ASS 格式下载 bilibili 的弹幕
+// @include     http://www.bilibili.com/video/av*
 // @include     http://www.bilibili.tv/video/av*
 // @include     http://bilibili.kankanews.com/video/av*
 // @updateURL   https://tiansh.github.io/us-danmaku/bilibili/bilibili_ASS_Danmaku_Downloader.meta.js
 // @downloadURL https://tiansh.github.io/us-danmaku/bilibili/bilibili_ASS_Danmaku_Downloader.user.js
-// @version     1.2
+// @version     1.3
 // @grant       GM_addStyle
 // @grant       GM_xmlhttpRequest
 // @run-at      document-start
