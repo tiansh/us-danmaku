2c2671406fdd0178a2a332766ec7d6cfa1faffda
tiansh <tiansheng111@gmail.com>
2014-12-28 02:07:19 +0800

    添加在线转换功能，可以直接从网页上转换弹幕文件。

---
+++ page/common.js
@@ -0,0 +1,22 @@
+﻿var gotFile = function (name, content) {
+  var danmaku = parseFile(content);
+  var ass = generateASS(setPosition(danmaku), {
+    'title': document.title,
+    'ori': name,
+  });
+  startDownload('\ufeff' + ass, name.replace(/\.[^.]*$/, '') + '.ass');
+};
+window.addEventListener('load', function () {
+  var upload = document.querySelector('#upload');
+  upload.addEventListener('change', function () {
+    var file = upload.files[0];
+    var name = file.name;
+    var reader = new FileReader();
+    if (file.size > (1 << 24)) error();
+    else reader.addEventListener('load', function () {
+      gotFile(name, reader.result);
+    });
+    reader.readAsText(file);
+    upload.value = '';
+  });
+});
